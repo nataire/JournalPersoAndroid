e7fd73e9b5853d33c2a3cf45fe4c842b4951df2d
nataire <chabcedr17@gmail.com>
2020-01-30 17:30:26 +0100

    envoi calendrier + button

--- app/src/main/java/com/example/JournalPerso/ui/calendrier/CalendrierFragment.java
+++ app/src/main/java/com/example/JournalPerso/ui/calendrier/CalendrierFragment.java
@@ -1,11 +1,29 @@
 package com.example.JournalPerso.ui.calendrier;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.JournalPerso.R;
 
 public class CalendrierFragment extends Fragment {
 
     private CalendrierViewModel calendrierViewModel;
 
-    public void onCreateView() {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        calendrierViewModel =
+                ViewModelProviders.of(this).get(CalendrierViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_calendrier, container, false);
+
+        return root;
     }
 }
