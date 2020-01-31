7c50f65329d80571fba86aaa45094f67a085abd0
nataire <chabcedr17@gmail.com>
2020-01-31 11:39:40 +0100

    ajout ecran création indicateur

--- app/src/main/java/com/example/JournalPerso/ModifyEspaceActivity.java
+++ app/src/main/java/com/example/JournalPerso/ModifyEspaceActivity.java
@@ -3,13 +3,33 @@ package com.example.JournalPerso;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ModifyEspaceActivity extends FragmentActivity {
 
+    FloatingActionButton buttonAjoutIndicteur;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify_espace);
+
+
+        buttonAjoutIndicteur =  findViewById(R.id.buttonAjoutIndicateur);
+
+        buttonAjoutIndicteur.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(ModifyEspaceActivity.this, createIndicateurActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/JournalPerso/createIndicateurActivity.java
@@ -0,0 +1,27 @@
+package com.example.JournalPerso;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class createIndicateurActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_indicateur);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerTypeIndicateur);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.typeIndicateur, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+    }
+}
